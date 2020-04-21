b43d98a70b4eb5025504609b5e89d489bf88852b
twinkleDing <twinkle415@163.com>
2020-04-21 13:46:09 +0800

    删除多余

--- miniprogram/pages/ji/ji.js
+++ miniprogram/pages/ji/ji.js
@@ -60,6 +60,24 @@ Page({
    */
   onLoad: function (options) {
     this.getDateTitle()
+    console.log(1)
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              console.log(res)
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
